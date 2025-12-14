028dfa336bfce9c5d17a9d91fe80d755d378976f
AnuBapatla <anusrichatala@gmail.com>
2025-12-14 15:26:25 -0500

    used map() with filter  for rendering list

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,18 +6,22 @@ export const ProductList =()=>{
            {id:4,name:"Watch",price:399},
             {id:5,name:"Headphone",price:199},  
         ];
-
+     const productElements = products.filter((product)=>{
+        return product.price >500;
+     })
+     .map((product)=>
+    {
+        return(
+            <div>
+                <h3>{(product.name)}</h3>
+            <p> Price:${product.price}</p>
+            </div>
+        );
+    });
     return(
         <div>
             <h2> Our products </h2>
-            {products.map((product)=>{
-                return(
-            <div> <h3> {(product.name)}</h3>
-            <p> Price:${product.price}</p>
+             {productElements}
             </div>
-            )
-        })
-            }
-        </div>
-    )
-}
+    );
+};
