8e108fbf3a9ebbcd55cf58c1c1a7627b48b604aa
AnuBapatla <anusrichatala@gmail.com>
2025-12-24 00:12:52 -0500

    use filter to remove lists and map to update items

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export const TodoList =()=> {
    const[items,setItems] =useState([
-        {id:1, text:"learn react"},
+        {id:1, text:"learn react",done:false},
         {id:2, text:"build an app"},
     ]);
     console.log("rendering with items",items);
@@ -10,17 +10,39 @@ export const TodoList =()=> {
         const newItem={
             id: Date.now(),
             text:"deploy to production",
+            done:false,
 
         };
         //setItems([...items,newItem]);
         setItems(items.concat(newItem));
     };
+    const removeItem =(id)=>{
+        setItems(items.filter((item) =>item.id !== id));
+    };
+    const toggleDone = (id) => {
+        setItems(items.map(item => {
+            if(item.id ===id){
+                return {...item,done: !item.done};
+            }
+            return item;
+        })
+    );
+};
     return(
         <div>
             <ul>
                 {items.map((item)=>
-                {return<li key={item.id}>{item.text}</li>;
-            })}
+                {
+                    return(
+                    <li key={item.id}>
+                <span style={{textDecoration:item.done?"line-through":"none"}}>{item.text}</span>
+                <button onClick={() => toggleDone(item.id)}>
+                    {item.done ? "undo" :"Done"}
+                </button>
+                <button onClick={() => removeItem(item.id)}>Delete</button>
+                </li>
+                );
+            })};
             </ul>
             <button onClick={addItem}> add item</button>
         </div>
